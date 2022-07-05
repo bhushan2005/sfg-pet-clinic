65590cf732ef16e8e1314e2b23256fbc614a6a33
bhushan2005 <bhushan2005@gmail.com>
2022-07-05 13:29:32 +0530

    POJOs . Closes #2

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -0,0 +1,33 @@
+package guru.springframework.sfgpetclinic.model;
+
+import java.time.LocalDate;
+
+public class Pet {
+    private PetType petTye;
+    private Owner owner;
+    private LocalDate birtdDate;
+
+    public PetType getPetTye() {
+        return petTye;
+    }
+
+    public void setPetTye(PetType petTye) {
+        this.petTye = petTye;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public LocalDate getBirtdDate() {
+        return birtdDate;
+    }
+
+    public void setBirtdDate(LocalDate birtdDate) {
+        this.birtdDate = birtdDate;
+    }
+}
