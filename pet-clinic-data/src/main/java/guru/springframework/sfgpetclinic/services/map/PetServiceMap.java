06cb0e7fcd4f23e5a8481d1573b4a7669de87b25
bhushan2005 <bhushan2005@gmail.com>
2022-07-06 16:27:38 +0530

    hasmap services

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,32 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import guru.springframework.sfgpetclinic.model.Pet;
+import guru.springframework.sfgpetclinic.services.PetService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+@Service
+public class PetServiceMap extends AbstractMapService<Pet, Long>
+        implements PetService {
+    @Override
+    public Pet save(Pet pet) {
+        return super.save(pet);
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
